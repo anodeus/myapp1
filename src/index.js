b35f2b48c6b1b6303f48b22b33fe0b1fda98f975
ABHI_SINGH_45 <74093124+anodeus@users.noreply.github.com>
2023-02-01 01:09:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from "./App";
 import "./index.css";
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-0", name: "Adfar Batch A 8:00am to 11:00am", completed: true },
+  { id: "todo-1", name: "Data Structure Practice 11:30am to 4:00pm", completed: false },
+  { id: "todo-2", name: "Adfar TA 6:30pm to 8:30pm", completed: false },
 ];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
